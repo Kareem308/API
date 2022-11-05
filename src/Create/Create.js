5e192bebba1c132a7c2807d925be3fc339cc39a6
Kareem <Kareem@test.com>
2022-11-05 23:36:01 +0300

    Clear

--- src/Create/Create.js
+++ src/Create/Create.js
@@ -1,29 +1,9 @@
-import React,{useState} from 'react'
-import axios from "axios";
+import React from 'react'
 
-function Create(){
-const [fName, setfName] = useState()
-const [lName, setlName] = useState()
-let url ="https://6362426266f75177ea2a9be0.mockapi.io/ToDo"
-
-const postData = () => {
-    axios.post(url,{
-        fName,
-        lName
-    }).then(res =>{
-        console.log(res);
-
-     })
-}
+function Create() {
   return (
-    <div>
-
-<input placeholder='FName' onChange={e=>{setfName(e.target.value)}}/>
-<input placeholder='FName' onChange={e=>{setlName(e.target.value)}}/>
-<button on onClick={postData}>LogIn</button>
-
-
-    </div>
+    <div>Create</div>
   )
 }
+
 export default Create
