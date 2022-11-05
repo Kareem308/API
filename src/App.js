89650ec03d134a5a2359cd0515863a9a8f2962a1
Kareem <Kareem@test.com>
2022-11-05 23:32:31 +0300

    Clear

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import React, {useEffect,useState} from "react";
-import axios from "axios";
-import Create from "./Create/Create";
-import './App.css';
+import React from 'react'
 
 function App() {
-  const [state, setstate] = useState([])
-
-  useEffect(()=>{
-    axios.get("https://6362426266f75177ea2a9be0.mockapi.io/ToDo").then((res) =>{
-      console.log(res);
-      setstate(res.data);
-    })
-  },[])
-  console.log(state)
-  
   return (
-    <div className="App">
-      
-      
-    <Create/>
-
-
-{state.map((e)=>(
-  <div>
-  <h1 >First Name:{e.fName} </h1>
-  <h1>Last Name:{e.fName}</h1>
-
-  </div>
-))}
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
